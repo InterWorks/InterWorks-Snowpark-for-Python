f2881c18e5677e77369470cdda8ec5483138ada5
Chris Hastie <Chris.Hastie@interworks.co.uk>
2023-01-05 12:04:52 +0000

    Support for Azure secrets vault

---
+++ interworks_snowpark_python/azure_secrets_vault_functions.py
@@ -0,0 +1,71 @@
+
+# Import the required modules for Azure
+from azure.keyvault.secrets import SecretClient
+from azure.identity import ManagedIdentityCredential
+
+# Import other required packages
+import os
+
+# Retrieve key vault URI
+def retrieve_key_vault_uri() :
+  key_vault_name = os.getenv("AZURE_KEY_VAULT_NAME")
+  key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
+
+  return key_vault_uri
+
+# Define function to leverage managed identity
+# to retrieve secret client
+def retrieve_secret_client() :
+  
+  managed_identity_credential = ManagedIdentityCredential()
+
+  key_vault_uri = retrieve_key_vault_uri()
+
+  secret_client = SecretClient(vault_url=key_vault_uri, credential=managed_identity_credential)
+
+  return secret_client
+
+# Define function to convert the given
+# key pair name to one that is compliant
+# with Azure Secrets naming conventions
+def protect_key_pair_name(
+      key_pair_name: str
+  ) :
+
+  ### Replace underscores with hyphens, as underscores
+  ### are expected in Snowflake service account usernames
+  protected_key_pair_name = key_pair_name.replace("_", "-")
+
+  return protected_key_pair_name
+
+# Define function to retrieve key pair
+# from the secrets vault
+def retrieve_private_key_from_azure_secrets(
+      secret_client: SecretClient
+    , private_key_name: str
+  ) :
+    
+  private_key = secret_client.get_secret(private_key_name).value
+  
+  return private_key
+
+# Define function to retrieve private key
+# from the secrets vault
+def retrieve_private_key_from_azure_secrets(
+      key_pair_name: str
+  ) :
+    
+  ### Leverage managed identity to retrieve secrets client
+  secret_client = retrieve_secret_client()
+  
+  ### Convert key pair name to one that is compliant
+  ### with Azure Secrets naming conventions
+  protected_key_pair_name = protect_key_pair_name(key_pair_name)
+
+  ### Retrieve the key pair from the vault
+  private_key = retrieve_private_key_from_azure_secrets(
+      secret_client = secret_client
+    , private_key_name = protected_key_pair_name
+  )
+  
+  return private_key
--- interworks_snowpark_python/snowpark_session_builder.py
+++ interworks_snowpark_python/snowpark_session_builder.py
@@ -34,7 +34,8 @@ def build_snowpark_session_via_parameters_json(
 
 ## Define function to build a Snowpark session
 ## leveraging a streamlit secrets file
-## containing Snowflake connection parameters
+## containing Snowflake connection parameters.
+## Only works if streamlit package is installed
 def build_snowpark_session_via_streamlit_secrets():
   
   ### Import required module
@@ -91,6 +92,48 @@ def build_snowpark_session_via_parameters_object(
   from .leverage_snowflake_connection_parameters_dictionary import retrieve_snowflake_connection_parameters
 
   ### Generate Snowflake connection parameters from the relevant source
+  snowflake_connection_parameters = retrieve_snowflake_connection_parameters(imported_connection_parameters, private_key_output_format = 'snowpark')
+
+  ### Create session to connect to Snowflake
+  snowpark_session = build_snowpark_session_from_connection_parameters(snowflake_connection_parameters)
+
+  return snowpark_session
+
+## Define function to build a Snowpark session
+## leveraging environment variables
+## containing Snowflake connection parameters,
+## with the private key stored in 
+## an Azure secrets vault.
+## Only works if corresponding Azure packages are installed
+def build_snowpark_session_using_stored_private_key_in_azure_secrets_vault() :
+  
+  ### Import required modules
+  from .azure_secrets_vault_functions import retrieve_private_key_from_azure_secrets
+  from .leverage_snowflake_connection_parameters_dictionary import retrieve_snowflake_connection_parameters
+  import os
+
+  ### Retrieve snowflake user from environment variables
+  snowflake_user = os.getenv("SNOWFLAKE_USER")
+
+  ### Retrieve private key for user from Azure Secrets Vault
+  private_key = retrieve_private_key_from_azure_secrets(snowflake_user)
+
+  ### Define empty variable that will be populated with JSON
+  snowflake_connection_parameters = {}
+
+  ### Define imported connection parameters using environment variables
+  ### whilst targeting specific account
+  imported_connection_parameters = {
+      "account" : os.getenv("SNOWFLAKE_ACCOUNT")
+    , "user" : snowflake_user
+    , "default_role" : os.getenv("SNOWFLAKE_DEFAULT_ROLE")
+    , "default_warehouse" : os.getenv("SNOWFLAKE_DEFAULT_WAREHOUSE")
+    , "default_database" : os.getenv("SNOWFLAKE_DEFAULT_DATABASE")
+    , "default_schema" : os.getenv("SNOWFLAKE_DEFAULT_SCHEMA")
+    , "private_key_plain_text" : private_key
+  }
+
+  ### Populate snowflake_connection_parameters from imported_connection_parameters
   snowflake_connection_parameters = retrieve_snowflake_connection_parameters(imported_connection_parameters, private_key_output_format = 'snowpark')
 
   ### Create session to connect to Snowflake
